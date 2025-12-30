6f3615f224dfe1f61b46771f233770d767438012
sonofpolaris1990 <247921456+sonofpolaris1990@users.noreply.github.com>
2025-12-30 23:42:25 +0100

    0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use gilrs::{Gilrs, Event};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Running fizzle ...");
+
+    let mut gilrs = Gilrs::new().expect("Failed to initialize gilrs");
+
+    println!("Connected gamepads:");
+    for (id, gamepad) in gilrs.gamepads() {
+        println!("{}: {}", id, gamepad.name());
+    }
+
+    loop {
+        while let Some(Event { id, event, time , ..}) = gilrs.next_event() {
+            println!("{:?} at {:?}: {:?}", id, time, event);
+        }
+    }
 }
